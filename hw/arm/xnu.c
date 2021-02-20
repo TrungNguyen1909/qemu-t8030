2bf5f98e6a50b9268dee2ced0b1204852043dade
Trung Nguyen <ntrung03@outlook.com>
2021-02-20 12:42:34 +0700

    T8030: Memory map

--- hw/arm/xnu.c
+++ hw/arm/xnu.c
@@ -430,7 +430,7 @@ static void macho_highest_lowest(struct mach_header_64* mh, uint64_t *lowaddr,
     *highaddr = high_addr_temp;
 }
 
-static void macho_file_highest_lowest(const char *filename, hwaddr *lowest,
+void macho_file_highest_lowest(const char *filename, hwaddr *lowest,
                                       hwaddr *highest)
 {
     gsize len;
@@ -443,39 +443,8 @@ static void macho_file_highest_lowest(const char *filename, hwaddr *lowest,
     g_free(data);
 }
 
-void macho_file_highest_lowest_base(const char *filename, hwaddr phys_base,
-                                    hwaddr *virt_base, hwaddr *lowest,
-                                    hwaddr *highest)
-{
-    uint8_t high_Low_dif_bit_index;
-    uint8_t phys_base_non_zero_bit_index;
-    hwaddr bit_mask_for_index;
-
-    macho_file_highest_lowest(filename, lowest, highest);
-    high_Low_dif_bit_index =
-        get_highest_different_bit_index(align_64k_high(*highest),
-                                        align_64k_low(*lowest));
-    if (phys_base) {
-        phys_base_non_zero_bit_index =
-            get_lowest_non_zero_bit_index(phys_base);
-
-        //make sure we have enough zero bits to have all the diffrent kernel
-        //image addresses have the same non static bits in physical and in
-        //virtual memory.
-        if (high_Low_dif_bit_index > phys_base_non_zero_bit_index) {
-            abort();
-        }
-        bit_mask_for_index =
-            get_low_bits_mask_for_bit_index(phys_base_non_zero_bit_index);
-
-        *virt_base = align_64k_low(*lowest) & (~bit_mask_for_index);
-    }
-
-}
-
 void arm_load_macho(char *filename, AddressSpace *as, MemoryRegion *mem,
-                    const char *name, hwaddr phys_base, hwaddr virt_base,
-                    hwaddr low_virt_addr, hwaddr high_virt_addr, hwaddr *pc)
+                    const char *name, hwaddr phys_base, hwaddr virt_base, hwaddr *pc)
 {
     uint8_t *data = NULL;
     gsize len;
@@ -484,19 +453,26 @@ void arm_load_macho(char *filename, AddressSpace *as, MemoryRegion *mem,
     if (!g_file_get_contents(filename, (char **)&data, &len, NULL)) {
         abort();
     }
+    
     struct mach_header_64* mh = (struct mach_header_64*)data;
     struct load_command* cmd = (struct load_command*)(data +
                                                 sizeof(struct mach_header_64));
 
-    uint64_t rom_buf_size = align_64k_high(high_virt_addr) - low_virt_addr;
-    rom_buf = g_malloc0(rom_buf_size);
     for (unsigned int index = 0; index < mh->ncmds; index++) {
         switch (cmd->cmd) {
             case LC_SEGMENT_64: {
                 struct segment_command_64 *segCmd =
                                             (struct segment_command_64 *)cmd;
-                memcpy(rom_buf + (segCmd->vmaddr - low_virt_addr),
-                       data + segCmd->fileoff, segCmd->filesize);
+                char region_name[32] = {0};
+                snprintf(region_name, sizeof(region_name), "%s-%s", name, segCmd->segname);
+                if(segCmd->vmsize == 0){
+                    break;
+                }
+                rom_buf = g_malloc0(segCmd->vmsize);
+                memcpy(rom_buf, data + segCmd->fileoff, segCmd->filesize);
+                allocate_and_copy(mem, as, region_name, phys_base + segCmd->vmaddr - virt_base, segCmd->vmsize, rom_buf);
+                g_free(rom_buf);
+                rom_buf = NULL;
                 break;
             }
             case LC_UNIXTHREAD: {
@@ -509,13 +485,8 @@ void arm_load_macho(char *filename, AddressSpace *as, MemoryRegion *mem,
         }
         cmd = (struct load_command*)((char*)cmd + cmd->cmdsize);
     }
-    hwaddr low_phys_addr = vtop_bases(low_virt_addr, phys_base, virt_base);
-    allocate_and_copy(mem, as, name, low_phys_addr, rom_buf_size, rom_buf);
 
     if (data) {
         g_free(data);
-    }
-    if (rom_buf) {
-        g_free(rom_buf);
     }
 }
