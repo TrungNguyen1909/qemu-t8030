5f8fe29c7bd50e7ab05100c91207d6394e163a48
Frederik Carlier <frederik.carlier@quamotion.mobi>
2021-02-22 21:10:01 +0100

    Fix build warnings

--- hw/intc/apple-aic.c
+++ hw/intc/apple-aic.c
@@ -211,7 +211,7 @@ static void apple_aic_write(void *opaque,
                     return;
             }
         }
-        fprintf(stderr, "AIC: Write to unspported reg 0x%llx\n", addr);
+        fprintf(stderr, "AIC: Write to unspported reg 0x%lx\n", addr);
     }
 }
 static uint64_t apple_aic_read(void *opaque,
@@ -275,7 +275,7 @@ static uint64_t apple_aic_read(void *opaque,
             }
         }
     }
-    fprintf(stderr, "AIC: Read from unspported reg 0x%llx\n", addr);
+    fprintf(stderr, "AIC: Read from unspported reg 0x%lx\n", addr);
     return -1;
 }
 
