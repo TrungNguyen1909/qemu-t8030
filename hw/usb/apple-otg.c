de4652b4b527706d86eb57bb961fb6261c6a1491
Trung Nguyen <ntrung03@outlook.com>
2021-05-17 10:03:54 +0700

    apple-otg: coding styles

--- hw/usb/apple-otg.c
+++ hw/usb/apple-otg.c
@@ -8,6 +8,7 @@
 #include "qemu/module.h"
 #include "qemu/timer.h"
 #include "hw/arm/xnu_dtb.h"
+#include "hw/usb/hcd-dwc2.h"
 
 #define rAUSB_USB20PHY_CTL		(0x00)
 #define rAUSB_USB20PHY_OTGSIG	(0x04)
@@ -18,7 +19,7 @@
 
 static void apple_otg_realize(DeviceState *dev, Error **errp)
 {
-
+    
 }
 static void apple_otg_reset(DeviceState *dev)
 {
@@ -27,14 +28,16 @@ static void apple_otg_reset(DeviceState *dev)
 static void phy_reg_write(void *opaque,
                   hwaddr addr,
                   uint64_t data,
-                  unsigned size){
+                  unsigned size)
+{
     fprintf(stderr, "OTG: phy reg WRITE @ 0x" TARGET_FMT_plx " value: 0x" TARGET_FMT_plx "\n", addr, data);    
     AppleOTGState *s = APPLE_OTG(opaque);
     memcpy(s->phy_reg + addr, &data, size);
 }
 static uint64_t phy_reg_read(void *opaque,
                      hwaddr addr,
-                     unsigned size){
+                     unsigned size)
+{
     fprintf(stderr, "OTG: phy reg READ @ 0x" TARGET_FMT_plx "\n", addr);
     AppleOTGState *s = APPLE_OTG(opaque);
     uint64_t val = 0;
@@ -49,26 +52,31 @@ static const MemoryRegionOps phy_reg_ops = {
 static void usbctl_reg_write(void *opaque,
                   hwaddr addr,
                   uint64_t data,
-                  unsigned size){
+                  unsigned size)
+{
     fprintf(stderr, "OTG: usbctl reg WRITE @ 0x" TARGET_FMT_plx " value: 0x" TARGET_FMT_plx "\n", addr, data);    
     AppleOTGState *s = APPLE_OTG(opaque);
     memcpy(s->usbctl_reg + addr, &data, size);
 }
+
 static uint64_t usbctl_reg_read(void *opaque,
                      hwaddr addr,
-                     unsigned size){
+                     unsigned size)
+{
     fprintf(stderr, "OTG: usbctl reg READ @ 0x" TARGET_FMT_plx "\n", addr);
     AppleOTGState *s = APPLE_OTG(opaque);
     uint64_t val = 0;
     memcpy(&val, s->usbctl_reg + addr, size);
     return val;
 }
+
 static const MemoryRegionOps usbctl_reg_ops = {
     .write = usbctl_reg_write,
     .read = usbctl_reg_read,
 };
 
-DeviceState *apple_otg_create(void){
+DeviceState *apple_otg_create(void)
+{
     DeviceState *dev;
     SysBusDevice *sbd;
     AppleOTGState *s;
@@ -92,7 +100,7 @@ static void apple_otg_class_init(ObjectClass *klass, void *data)
     DeviceClass *dc = DEVICE_CLASS(klass);
     dc->realize = apple_otg_realize;
     dc->reset = apple_otg_reset;
-    dc->desc = "Apple Synopsys USB OTG";
+    dc->desc = "Apple Synopsys USB OTG Controller";
 }
 
 static const TypeInfo apple_otg_info = {
